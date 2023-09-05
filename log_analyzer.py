2cbe45adedb0dfb56988f78e252ae2071a98dfd8
Rashid <rash.sh.77@gmail.com>
2023-09-05 19:16:38 +0500

    added class TestLogAnalyzer and methods add_url, calc_stat, test for methods added. Tested succesfully

--- log_analyzer.py
+++ log_analyzer.py
@@ -10,6 +10,49 @@
 config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
+class LogStat:
+    def __init__(self) -> None:
+        self.log = dict()
+        self.stat = {
+            "count": 0,
+            "time_sum": 0.0,
+        }
+
+    def add_url(self, url: str, time: float):
+        stat = self.log.get(url)
+        if stat is None:
+            self.log.update(
+                {
+                    url: {
+                        "data": [
+                            time,
+                        ]
+                    }
+                }
+            )
+        else:
+            self.log[url]["data"].append(time)
+
+    def calc_stat(self):
+        total_count = 0
+        total_time_sum = 0.0
+        for v in self.log.keys():
+            self.log[v]["count"] = len(self.log[v]["data"])
+            time_sum = sum(self.log[v]["data"])
+            self.log[v]["time_sum"] = time_sum
+            self.log[v]["time_avg"] = time_sum / self.log[v]["count"]
+            self.log[v]["time_max"] = max(self.log[v]["data"])
+
+            total_count += self.log[v]["count"]
+            total_time_sum += self.log[v]["time_sum"]
+
+            self.log[v]["time_perc"] = time_sum / total_time_sum * 100
+            count_perc = self.log[v]["count"] / total_count * 100
+            self.log[v]["count_perc"] = count_perc
+        self.stat["count"] = total_count
+        self.stat["time_sum"] = total_time_sum
+
+
 def main():
     pass
 
--- tests.py
+++ tests.py
@@ -1,28 +1,48 @@
 import unittest
 
+from log_analyzer import LogStat
 from log_parser import get_link, get_resp_time
 
+right_log = [
+    '3.3.3.3 - - [234 5ghd] "GET a h" 10.0',
+    '"GET a h" 10.0',
+    '"GET a h" 10.0',
+    '"GET b h" 5.0',
+    '"GET b h" 3.0',
+]
+bad_log = [
+    '"GET HTTP/1.1" 10.0',
+    '"GET b h" A',
+]
 
-class myTest(unittest.TestCase):
-    right_log = [
-        '3.3.3.3 - - [234 5ghd] "GET a h" 10.0',
-        '"GET a h" 10.0',
-        '"GET a h" 10.0',
-        '"GET b h" 5.0',
-        '"GET b h" 3.0',
-    ]
-    bad_log = [
-        '"GET HTTP/1.1" 10.0',
-        '"GET b h" A',
-    ]
 
+class TestParser(unittest.TestCase):
     def test_get_link(self):
-        self.assertEqual(get_link(self.right_log[0]), "a")
-        self.assertEqual(get_link(self.right_log[-1]), "b")
+        self.assertEqual(get_link(right_log[0]), "a")
+        self.assertEqual(get_link(right_log[-1]), "b")
 
     def test_get_resp_time(self):
-        self.assertAlmostEqual(get_resp_time(self.right_log[0]), 10.0)
-        self.assertEqual(get_resp_time(self.bad_log[-1]), None)
+        self.assertAlmostEqual(get_resp_time(right_log[0]), 10.0)
+        self.assertEqual(get_resp_time(bad_log[-1]), None)
+
+
+class TestLogAnalyzer(unittest.TestCase):
+    log_stat = LogStat()
+
+    def test_add_url(self):
+        url = get_link(right_log[0])
+        time = get_resp_time(right_log[0])
+        self.log_stat.add_url(url, time)
+        self.assertEqual(len(self.log_stat.log), 1)
+
+        self.log_st = LogStat()
+
+        for line in right_log:
+            self.log_st.add_url(get_link(line), get_resp_time(line))
+
+        self.log_st.calc_stat()
+        self.assertEqual(self.log_st.stat["count"], len(right_log))
+        self.assertAlmostEqual(self.log_st.stat["time_sum"], 38.0)
 
 
 if __name__ == "__main__":
